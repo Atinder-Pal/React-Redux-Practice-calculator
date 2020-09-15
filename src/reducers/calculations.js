6d702a03e8e42638e40b9e351b3588808f0deb92
Atinder-Pal <atty.pal46@gmail.com>
2020-09-15 11:37:53 -0600

    created reducer to handle action of ading new calculation to existing calculation history in global state

---
+++ src/reducers/calculations.js
@@ -0,0 +1,23 @@
+/**
+ *  Redux Reducer to carry out the request to add new calculation to existing Array of Calculation History
+ */
+const calculationsReducer = (state = [], action ) => {
+
+    switch ( action.type ) {
+
+        case 'ADD_NEW_CALCULATION':
+            const newCalculation = {
+                calculation: action.payload
+            }
+        //Create copy of original state Array to get previous calculaton History in our new Array
+        const newCalculationHistory = [...state];
+        // Add new calculation to Calculation History
+        newCalculationHistory.push( newCalculation );
+        // Return the updated state 
+        return newCalculationHistory;
+
+    default:
+        return state;
+    }
+}
+export default calculationsReducer;
